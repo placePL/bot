a4cade439615c0f39d3471aac314826ab50d3711
Jakub <jakub@cubepotato.eu>
2022-04-03 20:35:29 +0200

    30 nowych kont

--- src/bot.ts
+++ src/bot.ts
@@ -21,6 +21,7 @@ export async function start(username: string, password: string, headless: boolea
     });
 
     page = await browser.newPage();
+    await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.83 Safari/537.36');
 
     let ok = false;
     while (!ok) {
